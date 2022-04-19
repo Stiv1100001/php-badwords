75e0490128e597844a39fc628806df819e3797bd
Stiv1100001 <bichicchi.stefano@gmail.com>
2022-04-19 15:39:30 +0200

    bonus

--- index.php
+++ index.php
@@ -11,7 +11,33 @@
 
     $toCensor = $_GET['cens'] ?? "";
 
-    var_dump($text);
-    var_dump(strlen($text));
-    var_dump(str_replace($toCensor, "***", $text));
+    // var_dump($text);
+    // var_dump(strlen($text));
+    // var_dump(str_replace($toCensor, "***", $text));
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-badwords</title>
+</head>
+<body>
+    <main>
+        <h1>Testo</h1>
+        <p>
+            <kbd><?php echo $text ?></kbd>
+        </p>
+        <h1>Lunghezza Testo</h1>
+        <p>
+            <kbd><?php echo strlen($text) ?></kbd>
+        </p>
+        <h1>Testo censurato</h1>
+        <p>
+            <kbd><?php echo str_replace($toCensor, "***", $text) ?></kbd>
+        </p>
+    </main>
+</body>
+</html>
